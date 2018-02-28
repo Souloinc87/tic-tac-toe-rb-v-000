9b8ff326b059d597409974cc122ccaba5027d967
Marcus  = Soulo <souloinc87@gmail.com>
2018-02-28 22:31:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,9 +30,9 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  player = "X"
+  
   if valid_move?(board, index)
-    move(board, index, player)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
